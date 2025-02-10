127ab9c23d1e7c4a3e190d523adfd44847221cc6
tharunkalluru <tharun.kalluru@contentstack.com>
2025-02-10 15:48:37 -0500

    Added Lytics at Edge

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import App from 'next/app';
 import Head from 'next/head';
-import Script from 'next/script';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import Layout from '../components/layout';
@@ -58,20 +57,6 @@ function MyApp(props: Props) {
         <title>Contentstack-Nextjs-Starter-App</title>
         {page?.seo && page.seo.enable_search_indexing && metaData(page.seo)}
       </Head>
-      <Script id="lytics-tracking" strategy="afterInteractive">
-        {`
-          !function(){"use strict";var o=window.jstag||(window.jstag={}),r=[];function n(e){o[e]=function(){for(var n=arguments.length,t=new Array(n),i=0;i<n;i++)t[i]=arguments[i];r.push([e,t])}}n("send"),n("mock"),n("identify"),n("pageView"),n("unblock"),n("getid"),n("setid"),n("loadEntity"),n("getEntity"),n("on"),n("once"),n("call"),o.loadScript=function(n,t,i){var e=document.createElement("script");e.async=!0,e.src=n,e.onload=t,e.onerror=i;var o=document.getElementsByTagName("script")[0],r=o&&o.parentNode||document.head||document.body,c=o||r.lastChild;return null!=c?r.insertBefore(e,c):r.appendChild(e),this},o.init=function n(t){return this.config=t,this.loadScript(t.src,function(){if(o.init===n)throw new Error("Load error!");o.init(o.config),function(){for(var n=0;n<r.length;n++){var t=r[n][0],i=r[n][1];o[t].apply(o,i)}r=void 0}()}),this}}();
-          jstag.init({
-            src: 'https://c.lytics.io/api/tag/a84fef4e65fe894eecb707074a47c0f2/latest.min.js',
-            pageAnalysis: {
-              dataLayerPull: {
-                disabled: true
-              }
-            }
-          });
-          jstag.pageView();
-        `}
-      </Script>
       <Layout
         header={header}
         footer={footer}
