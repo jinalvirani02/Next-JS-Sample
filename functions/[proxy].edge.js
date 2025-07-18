13948922e4fde7949f4e6652515c95f844a0323c
jinalvirani02 <jinal.virani@contentstack.com>
2025-07-18 16:01:23 +0530

    Redirect edge example 2

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,15 +1,13 @@
 export default async function handler(request, context) {
-  const redirectHosts = new Set([
-    'www.servicestreetcolorado.com',
-    'www.servicestreetgeorgia.com',
-    'www.servicestreettennessee.com',
-    'www.servicestreettexas.com',
-  ]);
+  const redirectHosts = [
+    'https://nextjs-launch-challenge-eee898.devcontentstackapps.com',
+    'https://nextjs-app-router-csr.devcontentstackapps.com/'
+  ];
 
   const url = new URL(request.url);
 
-  if (redirectHosts.has(url.hostname)) {
-    url.hostname = 'www.servicestreet.com';
+  if (redirectHosts.includes(url.hostname)) {
+    url.hostname = 'https://consent-test.devcontentstackapps.com/';
     return Response.redirect(url.toString(), 308); // permanent redirect
   }
 
