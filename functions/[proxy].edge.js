412b36edce6bfce7aef20c55115e350ddeabf9c6
jinalvirani02 <jinal.virani@contentstack.com>
2025-07-18 16:12:03 +0530

    Redirect edge example 3

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -5,11 +5,12 @@ export default async function handler(request, context) {
   ];
 
   const url = new URL(request.url);
-
+  console.log(url.hostname, "1");
   if (redirectHosts.includes(url.hostname)) {
     url.hostname = 'https://consent-test.devcontentstackapps.com/';
     return Response.redirect(url.toString(), 308); // permanent redirect
   }
+  console.log(url.hostname, "2");
 
   // Continue with normal request
   return fetch(request);
